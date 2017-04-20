62a06ec32136a3d1a45915d49622ab29f594c2cf
Andrzej <andrzej@applover.pl>
2017-04-20 15:00:11 +0200

    small changes

--- core/src/com/diti/helpthefallingpeople/screens/AbstractScreen.java
+++ core/src/com/diti/helpthefallingpeople/screens/AbstractScreen.java
@@ -3,6 +3,7 @@ package com.diti.helpthefallingpeople.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -12,13 +13,13 @@ import com.diti.helpthefallingpeople.HTFPGame;
  * Created by DiTi on 2017-04-09.
  */
 
-public abstract class AbstractScreen implements Screen {
-    protected HTFPGame game;
-    protected Stage stage;
-    // camera?
-    protected SpriteBatch spriteBatch;
+abstract class AbstractScreen implements Screen {
+    HTFPGame game;
+    Stage stage;
+    SpriteBatch spriteBatch;
+    private OrthographicCamera camera;
 
-    public AbstractScreen(HTFPGame game){
+    AbstractScreen(HTFPGame game) {
         this.game = game;
         stage = new Stage(new FitViewport(HTFPGame.WIDTH, HTFPGame.HEIGHT));
         spriteBatch = new SpriteBatch();
@@ -28,9 +29,32 @@ public abstract class AbstractScreen implements Screen {
 
     protected abstract void init();
 
+    private void clearScreen() {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void dispose() {
+        game.dispose();
+    }
+
+    private void createCamera() {
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, HTFPGame.WIDTH / 2, HTFPGame.HEIGHT / 2);
+        camera.update();
+    }
+
     @Override
     public void show() {
-
     }
 
     @Override
@@ -38,33 +62,14 @@ public abstract class AbstractScreen implements Screen {
         clearScreen();
     }
 
-    private void clearScreen() {
-        Gdx.gl.glClearColor(1,1,1,1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-    }
-
-    @Override
-    public void resize(int width, int height) {
-
-    }
-
     @Override
     public void pause() {
-
+        game.setPaused(true);
     }
 
     @Override
     public void resume() {
-
-    }
-
-    @Override
-    public void hide() {
-
-    }
-
-    @Override
-    public void dispose() {
-        game.dispose();
+        game.setPaused(false);
     }
 }
+
