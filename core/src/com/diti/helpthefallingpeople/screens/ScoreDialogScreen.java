1b41a9db870817b8b98f2b267bf5993254d3fceb
chalvagit <clv.chalva@gmail.com>
2017-06-17 00:36:46 +0200

    added Restart and Menu buttons

--- core/src/com/diti/helpthefallingpeople/screens/ScoreDialogScreen.java
+++ core/src/com/diti/helpthefallingpeople/screens/ScoreDialogScreen.java
@@ -1,6 +1,14 @@
 package com.diti.helpthefallingpeople.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.diti.helpthefallingpeople.HTFPGame;
 
 /**
@@ -10,6 +18,7 @@ import com.diti.helpthefallingpeople.HTFPGame;
 public class ScoreDialogScreen extends AbstractScreen {
 
     private Texture background;
+    private TextButton restartBtn, menuBtn;
 
     public ScoreDialogScreen(HTFPGame game) {
         super(game);
@@ -18,6 +27,54 @@ public class ScoreDialogScreen extends AbstractScreen {
     @Override
     protected void init() {
         background = new Texture("scoredialog_placeholder.jpg");
+        initButtons();
+    }
+
+    private void initButtons() {
+        // Table for Buttons
+        Table table = new Table();
+        table.setSize(HTFPGame.WIDTH, HTFPGame.HEIGHT);
+
+        // Button Skin
+        Skin skin = new Skin();
+        TextureAtlas buttonAtlas = new TextureAtlas(Gdx.files.internal("skin/ui-blue.atlas"));
+        skin.addRegions(buttonAtlas);
+
+        // Button font
+        BitmapFont font = new BitmapFont();
+
+        //Button style
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.font = font;
+        textButtonStyle.up = skin.getDrawable("button_04");
+
+        // Restart Button
+        restartBtn = new TextButton("Play again", textButtonStyle);
+        restartBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                game.setScreen(new GameplayScreen(game));
+            }
+        });
+        restartBtn.setDebug(true); //TODO turn off debug before releasing
+
+        // Menu Button
+        menuBtn = new TextButton("Menu", textButtonStyle);
+        menuBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                game.setScreen(new Menu(game));
+            }
+        });
+        menuBtn.setDebug(true); //TODO turn off debug before releasing
+
+        table.add(restartBtn).minWidth(120);
+        table.add().minWidth(120);
+        table.add(menuBtn).minWidth(60);
+        stage.addActor(table);
+        Gdx.input.setCatchBackKey(true);
     }
 
     @Override
