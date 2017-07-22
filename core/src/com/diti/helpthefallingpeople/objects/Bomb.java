222b815f37773f45f6db3b010e2e037c999082e2
chalvagit <clv.chalva@gmail.com>
2017-07-23 01:17:52 +0200

    added Bomb

---
+++ core/src/com/diti/helpthefallingpeople/objects/Bomb.java
@@ -0,0 +1,11 @@
+package com.diti.helpthefallingpeople.objects;
+
+/**
+ * Created by terebun on 2017-07-21.
+ */
+
+public class Bomb extends FallingObj {
+    public Bomb(float posX, float speed, String fileName, int frameCols, int frameRows) {
+        super(posX, speed, fileName, frameCols, frameRows);
+    }
+}
